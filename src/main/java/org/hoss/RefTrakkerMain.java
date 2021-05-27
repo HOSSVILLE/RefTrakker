3f50b6c26608c29ed09c272a55798d48b55c79b9
Carl <carl.hostrander00@gmail.com>
2021-05-26 21:40:30 -0500

    latest changes

--- src/main/java/org/hoss/RefTrakkerMain.java
+++ src/main/java/org/hoss/RefTrakkerMain.java
@@ -5,8 +5,6 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.services.sheets.v4.model.*;
 import com.google.api.services.sheets.v4.Sheets;
 
